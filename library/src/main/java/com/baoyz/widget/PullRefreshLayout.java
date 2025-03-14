2704cf544a8b5b1a9d79d19687e9a62445c3c7f2
Shashank Holla <you@example.com>
2025-03-13 19:14:09 -0700

    Update gradle and few other things to make it buildable in 2025

--- library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
+++ library/src/main/java/com/baoyz/widget/PullRefreshLayout.java
@@ -3,8 +3,10 @@ package com.baoyz.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
-import android.support.v4.view.MotionEventCompat;
-import android.support.v4.view.ViewCompat;
+import androidx.core.view.MotionEventCompat;
+import androidx.core.view.ViewCompat;
+//import android.support.v4.view.MotionEventCompat;
+//import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
@@ -17,6 +19,8 @@ import android.view.animation.Interpolator;
 import android.view.animation.Transformation;
 import android.widget.AbsListView;
 import android.widget.ImageView;
+
+import com.baoyz.pullrefreshlayout.R;
 
 import java.security.InvalidParameterException;
 
